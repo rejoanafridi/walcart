b474281ee08efebba8d2e6248490a5320a536a1a
rejoanafridi <rejoanafridi93@gmail.com>
2022-09-21 15:32:01 +0600

    initial

--- components/header/Nav.js
+++ components/header/Nav.js
@@ -2,11 +2,13 @@ import React from "react";
 import styles from "../../styles/Layoput.module.css";
 import listStyle from "../../styles/Listitem.module.css";
 import navStyle from "../../styles/Nav.module.css";
+
+import list from "../../styles/Common.module.css";
 const Nav = () => {
 	return (
 		<div className={styles.container}>
-			<div className={listStyle.list}>
-				<div className={listStyle.list}>
+			<div className={list.box}>
+				<div className={list.box_one}>
 					<li> About Us </li>
 					<span>|</span>
 					<li> My Account </li>
@@ -16,7 +18,7 @@ const Nav = () => {
 					<li> Contacts </li>
 				</div>
 
-				<div className={listStyle.list}>
+				<div className={list.box_two}>
 					<li>Order Tracking</li>
 					<li>
 						<form action="#">
@@ -38,7 +40,7 @@ const Nav = () => {
 					</li>
 				</div>
 			</div>
-			<div className={navStyle.flex}>
+			<div className={list.box}>
 				<div className={navStyle.nav_logo}>
 					<img src="/images/logo.png" alt="" />
 				</div>
@@ -69,6 +71,58 @@ const Nav = () => {
 						<img src="images/support.png" alt="" />
 						<img src="images/cart.png" alt="" />
 					</div>
+				</div>
+			</div>
+			<div className={navStyle.category_third}>
+				<div className={navStyle.category}>
+					<i class="fa-solid fa-bars"></i>
+					<li>
+						<a href="#">
+							Top Categories <span>&#9660;</span>{" "}
+						</a>
+						{/* <ul>
+							<li>
+								<a href="#"> medium categroy </a>
+							</li>
+							<li>
+								<a href="#"> small categroy </a>
+							</li>
+							<li>
+								<a href="#"> hot sale </a>
+							</li>
+							<li>
+								<a href="#"> new item </a>
+							</li>
+						</ul> */}
+					</li>
+				</div>
+				<div className={navStyle.category_items}>
+					<li>
+						<form action="#">
+							<select name="deals" id="deals">
+								<option value="exclusive deals">Exclusive Deals</option>
+								<option value="trending">Trending</option>
+							</select>
+						</form>
+					</li>
+					<li>
+						<form action="#">
+							<select name="watch" id="watch">
+								<option value="smart watch">Smart Watches</option>
+								<option value="Rolex">Rolex</option>
+							</select>
+						</form>
+					</li>
+					<li>Cell phone</li>
+					<li>Headphone</li>
+				</div>
+				<div className={navStyle.discount}>
+					<img src="images/discount_logo.png" alt="" />
+					<li>
+						<p>only this weekend</p>
+						<h2>Super discount</h2>
+					</li>
+					<span>&#8964;</span>
 				</div>
 			</div>
 		</div>
